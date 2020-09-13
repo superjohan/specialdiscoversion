032057ee0c816abc4bf1ed4dddae9360c95418ba
Johan Halin <johan@richie.fi>
2020-09-13 16:45:18 +0300

    Add dictionary.

---
+++ demo/DemoDictionary.swift
@@ -0,0 +1,40 @@
+//
+//  DemoDictionary.swift
+//  demo
+//
+//  Created by Johan Halin on 13.9.2020.
+//  Copyright © 2020 Dekadence. All rights reserved.
+//
+
+import Foundation
+
+struct DemoDictionary {
+    static let words = [
+        [
+            "Special",
+            "Super",
+            "Original",
+            "Disconet",
+            "European",
+            "American",
+            "Razormaid",
+            "Midnight",
+        ],
+        [
+            "Disco",
+            "12″",
+            "7″",
+            "Dance",
+            "Album",
+            "Single",
+            "Extended",
+            "Club",
+        ],
+        [
+            "Version",
+            "Mix",
+            "Edit",
+            "Remix",
+        ]
+    ]
+}
