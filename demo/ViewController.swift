2b3002998a6fd6dc436132120ca0fd69c145e639
Johan Halin <johan@richie.fi>
2020-09-14 19:27:17 +0300

    Remove alpha modification.

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -94,12 +94,11 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
             .max(by: { $1.count > $0.count })!
             .count
 
-        for i in 0..<labelCount {
+        for _ in 0..<labelCount {
             let label = UILabel()
             label.backgroundColor = .clear
             label.textColor = .black
             label.isHidden = true
-            label.alpha = 1.0 - (CGFloat(i) / CGFloat(labelCount))
             self.contentView.addSubview(label)
             self.wordLabels.append(label)
         }
