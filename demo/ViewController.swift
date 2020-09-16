70d248b3091fc36f36673c42a9798834c06d1afe
Johan Halin <johan@richie.fi>
2020-09-16 17:29:51 +0300

    Remove SceneKit bits.
    I experimented a bit with it and it looked like shit, so just skip it.

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -13,12 +13,11 @@ import Foundation
 
 typealias CurrentModifier = (modifier: Modifier, value1: CGFloat, value2: CGFloat, value3: CGFloat)
 
-class ViewController: UIViewController, SCNSceneRendererDelegate {
+class ViewController: UIViewController {
     let autostart = true
     
     let audioPlayer: AVAudioPlayer
-//    let sceneView = SCNView()
-//    let camera = SCNNode()
+
     let startButton: UIButton
     let qtFoolingBgView: UIView = UIView.init(frame: CGRect.zero)
     let contentView = UIView()
@@ -51,16 +50,7 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         } else {
             abort()
         }
-        
-        let camera = SCNCamera()
-        camera.zFar = 600
-//        camera.vignettingIntensity = 1
-//        camera.vignettingPower = 1
-//        camera.colorFringeStrength = 3
-//        camera.bloomIntensity = 1
-//        camera.bloomBlurRadius = 40
-//        self.camera.camera = camera // lol
-        
+
         let startButtonText =
             "\"special disco version\"\n" +
                 "by jumalauta\n" +
@@ -83,15 +73,11 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         self.startButton.addTarget(self, action: #selector(startButtonTouched), for: UIControl.Event.touchUpInside)
         
         self.view.backgroundColor = .black
-//        self.sceneView.backgroundColor = .black
-//        self.sceneView.delegate = self
-        
+
         self.qtFoolingBgView.backgroundColor = UIColor(white: 0.1, alpha: 1.0)
         
         // barely visible tiny view for fooling Quicktime player. completely black images are ignored by QT
         self.view.addSubview(self.qtFoolingBgView)
-        
-//        self.view.addSubview(self.sceneView)
 
         self.contentView.backgroundColor = self.yellow
         self.contentView.isHidden = true
@@ -145,8 +131,6 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         super.viewDidLoad()
 
         self.audioPlayer.prepareToPlay()
-        
-//        self.sceneView.scene = createScene()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -158,10 +142,6 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
             width: 2,
             height: 2
         )
-
-//        self.sceneView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
-//        self.sceneView.isPlaying = true
-//        self.sceneView.isHidden = true
 
         self.contentView.frame = self.view.bounds
         self.labelContainer.frame = self.view.bounds
@@ -191,15 +171,7 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         
         self.audioPlayer.stop()
     }
-    
-    // MARK: - SCNSceneRendererDelegate
 
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        // this function is run in a background thread.
-//        DispatchQueue.main.async {
-//        }
-    }
-    
     // MARK: - Private
     
     @objc
@@ -215,8 +187,6 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
     }
     
     fileprivate func start() {
-//        self.sceneView.isHidden = false
-        
         self.audioPlayer.play()
 
         self.contentView.isHidden = false
@@ -441,27 +411,5 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         for label in self.wordLabels {
             label.font = UIFont.systemFont(ofSize: CGFloat(fontSize), weight: .heavy)
         }
-    }
-
-    fileprivate func createScene() -> SCNScene {
-        let scene = SCNScene()
-        scene.background.contents = UIColor.black
-        
-//        self.camera.position = SCNVector3Make(0, 0, 58)
-//        
-//        scene.rootNode.addChildNode(self.camera)
-        
-        configureLight(scene)
-        
-        return scene
-    }
-    
-    fileprivate func configureLight(_ scene: SCNScene) {
-        let omniLightNode = SCNNode()
-        omniLightNode.light = SCNLight()
-        omniLightNode.light?.type = SCNLight.LightType.omni
-        omniLightNode.light?.color = UIColor(white: 1.0, alpha: 1.0)
-        omniLightNode.position = SCNVector3Make(0, 0, 60)
-        scene.rootNode.addChildNode(omniLightNode)
     }
 }
